0afd949aa34ea480ce3df02c135f127b51c46cdb
George Kettleborough <kettleg@gmail.com>
2023-10-08 15:50:42 +0100

    Update CI

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
 .PHONY: all
 all: build
 
+.PHONY: build
 build:
-	hugo
+	cd content-org && emacs --batch -Q --load ../publish.el --funcall gpk-publish-all
+	hugo --minify
 
+.PHONY: clean
+clean:
+	rm content/posts/*.md
+
+.PHONY: dev
 dev:
 	hugo server -D --navigateToChanged
---
+++ publish.el
@@ -0,0 +1,42 @@
+;;; publish.el --- publish org-mode blog                     -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; original influence: https://github.com/NethumL/nethuml.github.io/
+
+;;; Code:
+(defconst gpk-content-files
+  '("life.org"
+    "networking.org"
+    "programming.org"
+    "software.org"
+    "technology.org"
+    "thoughts.org"))
+
+;; Install packages
+(require 'package)
+(package-initialize)
+(unless package-archive-contents
+  (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-refresh-contents))
+(dolist (pkg '(org-contrib ox-hugo))
+  (package-install pkg))
+
+(require 'url-methods)
+(url-scheme-register-proxy "http")
+(url-scheme-register-proxy "https")
+
+(require 'org)
+(require 'ox-extra)
+(require 'ox-hugo)
+(ox-extras-activate '(ignore-headlines))
+
+(defun gpk-publish-all ()
+  "Publish all content files"
+  (message "Publishing from emacs...")
+  (dolist (file gpk-content-files)
+    (find-file file)
+    (org-hugo-export-wim-to-md t)
+    (message (format "Exported from %s" file)))
+  (message "Finished exporting to markdown"))
+
+;;; publish.el ends here
