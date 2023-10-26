8c072887b6739208f10da93d2805fd987569d200
George Kettleborough <kettleg@gmail.com>
2023-10-26 20:17:17 +0100

    Update map JS

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,25 +1,39 @@
 import * as pmtiles from "pmtiles";
 import * as maplibregl from "maplibre-gl";
 import layers from 'protomaps-themes-base';
-import * as params from '@params';
 
 let protocol = new pmtiles.Protocol();
 maplibregl.addProtocol("pmtiles",protocol.tile);
 
-var map = new maplibregl.Map({
-    container: 'map', // container id
-    style: {
-        version: 8,
-        glyphs: 'https://cdn.protomaps.com/fonts/pbf/{fontstack}/{range}.pbf',
-        sources: {
-            "protomaps": {
-                type: "vector",
-                url: `pmtiles://${params.pmtiles}`,
-                attribution: '<a href="https://protomaps.com">Protomaps</a> © <a href="https://openstreetmap.org">OpenStreetMap</a>'
-            }
+function makeMap({tilesUrl, bounds, maxBounds, container = "map"}) {
+    console.log(tilesUrl, bounds, maxBounds, container);
+    var map = new maplibregl.Map({
+        container: container,
+        style: {
+            version: 8,
+            glyphs: 'https://cdn.protomaps.com/fonts/pbf/{fontstack}/{range}.pbf',
+            sources: {
+                "protomaps": {
+                    type: "vector",
+                    url: `pmtiles://${tilesUrl}`,
+                    attribution: '<a href="https://protomaps.com">Protomaps</a> © <a href="https://openstreetmap.org">OpenStreetMap</a>'
+                }
+            },
+            layers: layers("protomaps","light")
         },
-        layers: layers("protomaps","light")
-    },
-    bounds: params.bounds.split(",").map(parseFloat),
-    maxBounds: params.maxBounds.split(",").map(parseFloat),
+        bounds: bounds,
+        maxBounds: maxBounds,
+    });
+    return map;
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelectorAll("div.map").forEach((e) => {
+        makeMap({
+            tilesUrl: e.dataset.tilesUrl,
+            bounds: e.dataset.bounds.split(",").map(parseFloat),
+            maxBounds: e.dataset.maxBounds.split(",").map(parseFloat),
+            container: e,
+        });
+    });
 });
