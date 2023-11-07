7ec2a72b1129a8ab06f9b5dab3082758b87662a4
georgek <kettleg@gmail.com>
2023-11-07 20:41:38 +0000

    Deploying to gh-pages from @ georgek/blog@ac7dcc506b1b41b721dd23a174e72f8f41f5cc52 🚀

---
+++ js/early.min.8bbbb9e374f9a31ff012ccd8e17b750da15211875e9e896d6ea765d7e3c2179a.js
@@ -0,0 +1,1 @@
+(()=>{localStorage.theme?document.documentElement.setAttribute("data-theme",localStorage.theme):window.matchMedia("(prefers-color-scheme: dark)").matches&&document.documentElement.setAttribute("data-theme","dark")})()
